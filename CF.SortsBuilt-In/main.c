54a56b18a62fc4fa1c91f661c1014a91d2d6f5e9
Alan Sampson <alansamps@gmail.com>
2020-04-04 15:44:41 -0700

    Initial Commit

---
+++ CF.SortsBuilt-In/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CF.SortsBuilt-In
+//
+//  Created by Alan Sampson on 4/4/20.
+//  Copyright © 2020 Alan @ FreeShell. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
